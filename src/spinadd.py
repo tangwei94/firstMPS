c693ade67ba74c2245aea38634e748a1c05c805f
Wei Tang <tangwei@smail.nju.edu.cn>
2019-01-23 16:42:33 +0800

    fix a mistake in spinadd

--- src/spinadd.py
+++ src/spinadd.py
@@ -32,24 +32,24 @@ def init_newpsi(sigma, As, Bs): # not tested
     sigma1 = obtain_sigma1(sigma, Bs)
     sigma = np.diag(sigma)
 
+    maxdim = Bs[0].shape[0]
+
     matprod_l = np.asarray([sigma.dot(Bs[0]), sigma.dot(Bs[1])])
     shape_l = matprod_l.shape
     matprod_l = np.reshape(matprod_l, (shape_l[0]*shape_l[1], shape_l[2]))
-    if (matprod_l.shape[0] > matprod_l.shape[1]):
-        diff_shape = (matprod_l.shape[0], matprod_l.shape[0] - matprod_l.shape[1])
+    if (maxdim > matprod_l.shape[1]):
+        diff_shape = (matprod_l.shape[0], maxdim - matprod_l.shape[1])
         matprod_l = np.append(matprod_l, np.zeros(diff_shape, dtype=np.complex), axis=1)
     u, lambdaR = np.linalg.qr(matprod_l)
     newAs = np.reshape(u, (2, u.shape[0]//2, u.shape[1]))
-
-    dim = matprod_l.shape[0]
 
     matprod_r = np.asarray([As[0].dot(sigma), As[1].dot(sigma)])
     matprod_r = np.swapaxes(matprod_r, 0, 1)
     shape_r = matprod_r.shape
     matprod_r = np.reshape(matprod_r, (shape_r[0], shape_r[1]*shape_r[2]))
     matprod_r_dag = matprod_r.T.conj()
-    if (matprod_r_dag.shape[0] > matprod_r_dag.shape[1]):
-        diff_shape = (matprod_r_dag.shape[0], matprod_r_dag.shape[0] - matprod_r_dag.shape[1])
+    if (maxdim > matprod_r_dag.shape[1]):
+        diff_shape = (matprod_r_dag.shape[0], maxdim - matprod_r_dag.shape[1])
         matprod_r_dag = np.append(matprod_r_dag, np.zeros(diff_shape, dtype=np.complex), axis=1)
     v, lambdaL_dag = np.linalg.qr(matprod_r_dag)
     vdag, lambdaL = v.T.conj(), lambdaL_dag.T.conj()
@@ -63,8 +63,8 @@ def init_newpsi(sigma, As, Bs): # not tested
             return 0
     calc_rvs = np.frompyfunc(rvs, 1, 1)
     sigma1_rvsd = calc_rvs(sigma1)
-    if len(sigma1_rvsd) < dim:
-        sigma1_rvsd = np.append(sigma1_rvsd, np.zeros(dim - len(sigma1_rvsd)))
+    if len(sigma1_rvsd) < maxdim:
+        sigma1_rvsd = np.append(sigma1_rvsd, np.zeros(maxdim - len(sigma1_rvsd)))
     sigma1_rvsd = np.diag(calc_rvs(sigma1_rvsd))
 
     newlambda = lambdaR.dot(sigma1_rvsd).dot(lambdaL)
