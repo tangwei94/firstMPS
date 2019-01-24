5053be8d1e8dd34818871620ce361f3133681ac1
Wei Tang <tangwei@smail.nju.edu.cn>
2019-01-24 20:50:11 +0800

    idmrg.py

--- src/idmrg.py
+++ src/idmrg.py
@@ -3,20 +3,23 @@ from numpy import linalg
 import scipy
 from scipy import sparse
 from scipy.sparse import linalg
-import sys
+import warnings
 import ed, mpo
 import contraction as contr
 import spinadd as sadd
 from canonical import canonical
+import h5py
 
 class idmrg(object):
     def __init__(self, bondD, targetL):
         self.bondD = bondD
-        self.targetL = targetL
         self.M = mpo.xy_mpo()
-
+        self.targetL = targetL
         if self.targetL % 2 != 0: 
-            sys.exit('L must be even')
+            warnings.warn('idmrg: L must be even. L modified to L+1')
+            self.targetL += 1
+
+        self.dataname = 'xy_dmrg_D'+str(self.bondD)+'.h5'
 
         initL = 2*int(np.log2(self.bondD)) + 2
         self.currL = initL
@@ -24,16 +27,20 @@ class idmrg(object):
         H0 = ed.xy_hamilt(initL)
         psi0 = ed.ground_state(H0)[1]
         A_list, B_list, sigma = canonical(psi0, initL)
-
+        
         # truncate
         A_list[initL//2 - 1] = np.compress([1]*self.bondD, A_list[initL//2 - 1], axis=2)
         B_list[initL//2 - 1] = np.compress([1]*self.bondD, B_list[initL//2 - 1], axis=1)
         sigma = np.compress([1]*self.bondD, sigma)
+        if not contr.check_Lnorm(A_list) or not contr.check_Rnorm(B_list):
+            warnings.warn('idrmg: normalization condition violated due to truncation')
 
         self.As, self.Bs = A_list[initL//2 - 1], B_list[initL//2 - 1]
-        self.As1, self.Bs1 = A_list[initL//2 - 2], B_list[initL//2 - 2]
-        
         self.sigma = sigma
+
+        hf = h5py.File(self.dataname, 'w')
+        hf['parameters/bondD']=self.bondD
+        hf['parameters/currL']=self.currL
 
         self.Cl = np.reshape([1,0,0,0], (1,1,4,1)) 
         self.Cr = np.reshape([0,0,0,1], (1,4,1,1))
@@ -41,50 +48,73 @@ class idmrg(object):
             self.Cl = contr.contractL(self.Cl, A_list[ix], self.M)
             self.Cr = contr.contractR(self.Cr, B_list[ix], self.M)
 
-    def growby4(self):
-        newAs, newBs, initpsi = sadd.init_newpsi(self.sigma, self.As, self.As1, self.Bs, self.Bs1)
+            # save As, Bs, Cl, Cr
+            hf['data/As_'+str(ix)] = A_list[ix].flatten()
+            hf['data/As_'+str(ix)].attrs['shape'] = A_list[ix].shape
+            hf['data/Cl_'+str(ix)] = self.Cl.flatten()
+            hf['data/Cl_'+str(ix)].attrs['shape'] = self.Cl.shape
+            hf['data/Bs_'+str(ix)] = B_list[ix].flatten()
+            hf['data/Bs_'+str(ix)].attrs['shape'] = B_list[ix].shape        
+            hf['data/Cr_'+str(ix)] = self.Cr.flatten()
+            hf['data/Cr_'+str(ix)].attrs['shape'] = self.Cr.shape
+        hf['data/sigma'] = self.sigma
+
+        hf.close()
+
+    def growby2(self):
+        newAs, newBs, initpsi = sadd.init_newpsi(self.sigma, self.As, self.Bs)
         newCl = contr.contractL(self.Cl, newAs, self.M)
         newCr = contr.contractR(self.Cr, newBs, self.M)
-        
-        newClH = np.einsum('adef,bceg->abdgcf', newCl, self.M)
-        newCrH = np.einsum('abce,defz->adcbfz', self.M, newCr)
 
-        shapeCl = newClH.shape
-        newClH = np.reshape(newClH, (shapeCl[1]*shapeCl[2], shapeCl[3], shapeCl[4]*shapeCl[5]))
-        shapeCr = newCrH.shape
-        newCrH = np.reshape(newCrH, (shapeCr[0]*shapeCr[1], shapeCr[2], shapeCr[3]*shapeCr[4]))
-
-        H2 = np.einsum('acd,bce->abde', newClH, newCrH)
-        dimH2 = H2.shape[0]*H2.shape[1]
+        H2 = np.einsum('abde,cdfg->abcefg', newCl, newCr)
+        dimH2 = newCl.shape[1]**2
         H2 = np.reshape(H2, (dimH2, dimH2))
         H2s = scipy.sparse.csr_matrix(H2)
         
         if not np.allclose(H2, H2.T.conj()):
-            sys.exit(str(self.currL)+': H2 not Hermitian')
+            warnings.warn(str(self.currL)+': H2 not Hermitian')
 
         initpsi = np.reshape(initpsi, (dimH2, 1))
         newpsi = scipy.sparse.linalg.eigsh(H2s, k=1, which='SA', v0=initpsi)[1]
-        #newpsi = scipy.sparse.linalg.eigsh(H2s, k=1, which='SA')[1]
-        newpsi = np.reshape(newpsi, (newClH.shape[0], newClH.shape[0]))
+        newpsi = np.reshape(newpsi, (newCl.shape[1], newCl.shape[1]))
         u, s, vdag = np.linalg.svd(newpsi)
 
         # update
-        self.As = np.reshape(u, (2, u.shape[0]//2, u.shape[1]))
-        self.As1 = newAs
-        self.Bs = np.reshape(vdag, (vdag.shape[0], 2, vdag.shape[1]//2))
-        self.Bs = np.swapaxes(self.Bs, 0, 1)
-        self.Bs1 = newBs
+        self.currL += 2
+        self.As = np.einsum('abc,cd->abd', newAs, u)
+        self.Bs = np.einsum('ac,bcd->bad', vdag, newBs)
         self.sigma = s
 
-        self.Cl = contr.contractL(newCl, self.As, self.M)
-        self.Cr = contr.contractR(newCr, self.Bs, self.M)
+        self.Cl = np.einsum('abcd,be->aecd', newCl, u.conj())
+        self.Cl = np.einsum('abcd,de->abce', self.Cl, u)
+        self.Cr = np.einsum('ab,bcde->acde', vdag.conj(), newCr)
+        self.Cr = np.einsum('ad,bcde->bcae', vdag, self.Cr)
+
+        # save data
+        hf = h5py.File(self.dataname, 'r+')
+        ix = self.currL // 2 - 1
+        hf['data/As_'+str(ix)] = self.As.flatten()
+        hf['data/As_'+str(ix)].attrs['shape'] = self.As.shape
+        hf['data/Cl_'+str(ix)] = self.Cl.flatten()
+        hf['data/Cl_'+str(ix)].attrs['shape'] = self.Cl.shape
+        hf['data/Bs_'+str(ix)] = self.Bs.flatten()
+        hf['data/Bs_'+str(ix)].attrs['shape'] = self.Bs.shape        
+        hf['data/Cr_'+str(ix)] = self.Cr.flatten()
+        hf['data/Cr_'+str(ix)].attrs['shape'] = self.Cr.shape
+
+        del hf['parameters/currL']
+        hf['parameters/currL'] = self.currL
+        del hf['data/sigma']
+        hf['data/sigma'] = self.sigma
+
+        hf.close()
 
 if __name__ == '__main__':
-    bondD = 6
+    bondD = 20
     targetL = 100
     hello = idmrg(bondD, targetL)
 
-    for ix in range(200):
-        print(ix)
-        hello.growby4()
-        print(ix, np.around(hello.sigma, 4))
+    for ix in range(100):
+        hello.growby2()
+        hello.growby2()
+        print(ix, np.around(hello.sigma, 5))
