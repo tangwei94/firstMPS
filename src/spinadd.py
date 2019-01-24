992054fbbc3c611b80a449599bec321424057941
Wei Tang <tangwei@smail.nju.edu.cn>
2019-01-24 20:49:55 +0800

    fixes in spinadd.py

--- src/spinadd.py
+++ src/spinadd.py
@@ -26,7 +26,7 @@ def obtain_newAs(sigma, Bs):
     Bsfilled = fillzeros(Bs, dim)
     mat_prod = np.einsum('bc,acd->abd', np.diag(sigma), Bsfilled)
     mat_prod = np.reshape(mat_prod, (mat_prod.shape[0]*mat_prod.shape[1], mat_prod.shape[2]))
-    u, sr, vdag = np.linalg.svd(mat_prod)
+    u, sr, vdag = np.linalg.svd(mat_prod, full_matrices=False)
     newAs = np.reshape(u, (2, u.shape[0]//2, u.shape[1]))
     lambdaR = np.diag(sr).dot(vdag)
     return newAs, sr, lambdaR
@@ -36,7 +36,7 @@ def obtain_newBs(sigma, As):
     Asfilled = fillzeros(As, dim)
     mat_prod = np.einsum('abc,cd->bad', Asfilled, np.diag(sigma))
     mat_prod = np.reshape(mat_prod, (mat_prod.shape[0], mat_prod.shape[1]*mat_prod.shape[2]))
-    u, sl, vdag = np.linalg.svd(mat_prod)
+    u, sl, vdag = np.linalg.svd(mat_prod, full_matrices=False)
     newBs = np.reshape(vdag, (vdag.shape[0], 2, vdag.shape[1]//2))
     newBs = np.swapaxes(newBs, 0, 1)
     lambdaL = u.dot(np.diag(sl))
@@ -45,9 +45,6 @@ def obtain_newBs(sigma, As):
 def init_newpsi(sigma, As, Bs):
     newAs, sr, lambdaR = obtain_newAs(sigma, Bs)
     newBs, sl, lambdaL = obtain_newBs(sigma, As)
-
-    if not np.allclose(sl, sr, atol=1.e-5):
-        warnings.warn('init_newpsi: sl != sr')
 
     calc_rvs = np.frompyfunc(rvs, 1, 1)
     sr_rvsd = calc_rvs(sr)
